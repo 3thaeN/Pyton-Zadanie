33e56a5343a6666e6d3d73029fabcc3a1b231e44
3thaeN <kubao544@gmail.com>
2023-12-19 04:34:57 +0100

    Pierwszy commit

---
+++ task_manager.py
@@ -0,0 +1,104 @@
+import json
+import os
+
+class DziennikZajec:
+    def __init__(self, filename='journal.json'):
+        self.filename = filename
+        self.journal = self.read_journal()  # Poprawione: 'tasks' na 'journal'
+    
+    def read_journal(self):
+        journal = []
+        try:
+            if os.path.exists(self.filename):
+                with open(self.filename, 'r') as file:
+                    journal = json.load(file)  # Poprawione: 'journal.load' na 'json.load'
+        except FileNotFoundError:
+            print("Wystąpił błąd! Nie odnaleziono pliku!")
+        return journal
+    
+    def save_to_file(self):
+        try:
+            with open(self.filename, 'w') as file:  # Poprawione: 'r' na 'w'
+                json.dump(self.journal, file)
+        except FileNotFoundError:
+            print("Plik nie istnieje")
+    
+    def add_task(self, task):
+        self.journal.append(task)  # Poprawione: 'task' na 'journal'
+        self.save_to_file()
+        print("Zadanie zapisane w pliku!")
+    
+    def edit_task(self, number, new_task):
+        try:
+            if 0 <= number < len(self.journal):
+                self.journal[number] = new_task
+                self.save_to_file()
+                print("Zadanie zedytowane poprawnie!")
+            else:
+                raise ValueError("Zadanie o takim numerze nie istnieje!")  # Poprawione: Wyjątek ValueError
+        except ValueError as e:
+            print(f"Błąd: {e}")
+    
+    def remove_task(self, number):
+        try:
+            if 0 <= number < len(self.journal):
+                removed_task = self.journal.pop(number)
+                self.save_to_file()
+                print("Zadanie usunięte!")
+            else:
+                raise ValueError("Zadanie o takim numerze nie istnieje!")  # Poprawione: Wyjątek ValueError
+        except ValueError as e:
+            print(f"Błąd: {e}")
+
+def menu():
+    print("\n Lista Zadań")
+    print("1. Dodaj zadanie")
+    print("2. Edytuj zadanie")
+    print("3. Usuń zadanie")
+    print("4. Zapisz zadania do pliku")
+    print("5. Odczytaj zadania z pliku")
+    print("6. Wyjdź")
+
+def perform_actions(tasks_manager, action):  # Poprawione: Przekazanie tasks_manager jako argumentu
+    if action == 1:
+        task_content = input("Podaj treść zadania: ")
+        tasks_manager.add_task(task_content)
+    elif action == 2:
+        try:
+            task_number = int(input("Podaj numer zadania do edycji: "))
+            new_task_content = input("Podaj nową treść zadania: ")
+            tasks_manager.edit_task(task_number, new_task_content)
+        except ValueError:
+            print("Błędny numer zadania.")
+    elif action == 3:
+        try:
+            task_number = int(input("Podaj numer zadania do usunięcia: "))
+            tasks_manager.remove_task(task_number)
+        except ValueError:
+            print("Błędny numer zadania.")
+    elif action == 4:
+        tasks_manager.save_to_file()
+        print("Zadania zapisane do pliku.")
+    elif action == 5:
+        tasks_manager.journal = tasks_manager.read_journal()
+        print("Zadania odczytane z pliku.")
+
+if __name__ == "__main__":
+    tasks_manager = DziennikZajec()  # Poprawione: Utworzenie instancji klasy DziennikZajec
+
+    while True:
+        menu()
+        choice = input("Jaką operację chcesz wykonać? (1-6): ")
+
+        if choice == '6':
+            print("Do zobaczenia!")
+            break
+
+        try:
+            action = int(choice)
+            if 1 <= action <= 6:  # Poprawione: '5' na '6'
+                perform_actions(tasks_manager, action)
+            else:
+                print("Niepoprawny numer!")
+        except ValueError:
+            print("Niepoprawny numer!")
